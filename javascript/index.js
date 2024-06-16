462e967356bd380ba6c78bdba883b4c970acaa4d
kristijanpanevski <kristijanpanevski@yahoo.com>
2024-06-16 22:24:52 +0200

    Header added

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,78 @@
 // Data url
 const dataURL = "/data/posts.json";
 
+// Header
+// Hamburger menu
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((i) => i.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+// Function to open the login popup
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+// Function to close the login popup
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+
+// Function to open the newsletter popup
+function openNewsletter() {
+  document.querySelector(".newsletterPopup").style.display = "flex";
+}
+
+// Function to close the newsletter popup
+function closeNewsletter() {
+  document.querySelector(".newsletterPopup").style.display = "none";
+}
+
+// Event listeners for login popup
+document
+  .querySelector(".open-button")
+  .addEventListener("click", function (event) {
+    event.stopPropagation();
+    openForm();
+  });
+
+// Event listeners for newsletter popup
+document
+  .getElementById("NewsletterPopUp")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    openNewsletter();
+  });
+
+document
+  .querySelector(".newsletterPopup .close-button")
+  .addEventListener("click", closeNewsletter);
+
+// Close the login popup when clicking outside of it
+window.addEventListener("click", function (event) {
+  const form = document.getElementById("myForm");
+  if (
+    event.target !== form &&
+    !form.contains(event.target) &&
+    form.style.display === "block"
+  ) {
+    closeForm();
+  }
+});
 // Selctors
 const postsContainer = document.querySelector(".main-posts-container");
 const trendingPostsContainer = document.querySelector(
