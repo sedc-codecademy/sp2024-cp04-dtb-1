6e356f90155a8dd2bbcdcb8fd0aa7514b2d2cc87
Bojan Jankov <jankovbojan4@gmail.com>
2024-06-16 13:37:46 +0200

    added tag in every post in data, added render for two trending posts with most stars rating

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,9 @@ const dataURL = "/data/posts.json";
 
 // Selctors
 const postsContainer = document.querySelector(".main-posts-container");
+const trendingPostsContainer = document.querySelector(
+  ".trending-posts-container"
+);
 const showMoreButton = document.querySelector(".load-more-button");
 
 // Fetch function for posts data in homepage
@@ -13,6 +16,7 @@ const fetchPosts = async () => {
     const data = await response.json();
 
     console.log(data);
+    renderTrendingPosts(data);
     renderPosts(data);
 
     return data;
@@ -58,6 +62,49 @@ const renderPosts = (posts) => {
 
     postsContainer.innerHTML = postHTML;
   });
+};
+
+// Render trending posts functionality
+
+const renderTrendingPosts = (posts) => {
+  let trendingPostsHTML = "";
+
+  posts
+    .filter((post) => post.rating >= 5)
+    .slice(0, 2)
+    .map((post) => {
+      trendingPostsHTML += `<div class="post-container">
+            <div class="blog-post">
+              <div class="blog-post_img">
+                <img
+                  src=${post.image}
+                  alt=""
+                  height="45px"
+                />
+              </div>
+              <div class="blog-post_info">
+                <h1 class="blog-post_title">${post.title}</h1>
+                <p class="blog-post_text">
+                ${post.description}
+                </p>
+                <div class="blog-post_button_and_date">
+                  <div>
+                    <button class="blog-post_button">Read More</button>
+                  </div>
+                  <div class="blog-post_date">
+                    <div>
+                      <small>${post.author}</small>
+                      <small>${post.date}</small>
+                    </div>
+                    <small class="post-rating">${post.rating}⭐</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    });
+
+  trendingPostsContainer.innerHTML += trendingPostsHTML;
 };
 
 // Button show more functionality
