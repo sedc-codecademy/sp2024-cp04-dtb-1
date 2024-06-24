4ee23c45eb6f1e50c2c8290e6f4eec9aee821d2c
Marko1703 <marko.andonov94@gmail.com>
2024-06-25 00:53:09 +0200

    Register page added

---
+++ javascript/register-page.js
@@ -0,0 +1,20 @@
+function validation() {
+    if (document.Formfill.fName.value.length<3) {
+        document.getElementById("result").innerHTML="First name should be atleast 3 letters*";
+        return false;
+    } else if (document.Formfill.lName.value.length<3) {
+        document.getElementById("result").innerHTML="Last name should be atleast 3 letters*";
+        return false;
+    } else if (document.Formfill.password.value.length<8) {
+        document.getElementById("result").innerHTML="Password should be atleast 8 characters*";
+        return false;
+    } else if (document.Formfill.cPassword.value !== document.Formfill.password.value) {
+        document.getElementById("result").innerHTML="Password does not match*";
+        return false;
+    } else if (document.Formfill.password.value == document.Formfill.cPassword.value) {
+        registered.classList.add('open-slide')
+        return false;
+    } 
+}
+
+const registered = document.getElementById("#registered");
